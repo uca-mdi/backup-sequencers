23babf0ab10a86af878c46d53872e4ca9ad52aa3
marcomilanesio <milanesio.marco@gmail.com>
2018-02-07 16:36:53 +0100

    first commit

---
+++ backup.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+WHO=""  # to change accordingly
+
+SRCRES=/results/analysis/output/Home
+SRCPLU=/results/plugins
+
+ENTRYPOINT=/home/ionadmin/NAS   # make sure it's mounted
+DST=${ENTRYPOINT}/backup-sequencers/${WHO}
+DSTRES=${DST}/results
+DSTARC=${DST}/archive
+DSTPLU=${DST}/plugins
+
+
+mountpoint -q ${ENTRYPOINT}
+
+if [[ $? -eq 0 ]]; then
+    echo "preparing to backup..."
+else
+    echo "no mount point found. Aborting"
+    exit 1
+fi
+
+if [ ! -d $DST ]; then
+	mkdir -p $DST
+fi
+
+if [ ! -d $DSTARC ]; then
+	mkdir $DSTARC
+fi
+
+if [ ! -d $DSTRES ]; then
+	mkdir $DSTRES
+fi
+
+if [ ! -d $DSTPLU ]; then
+	mkdir $DSTPLU
+fi
+
+
+echo "Preparing tranfer results => src:'${SRCRES}'; dst:'${DSTRES}'"
+# -a does not work due to windows permissions on NAS
+# --no-links for avoiding broken links
+# --prune-empty-dirs (e.g., "plugins.out/")
+rsync -rlDzmv --no-links --prune-empty-dirs --dry-run --delete-excluded --delete-during --exclude='*_tn_*' $SRCRES $DSTRES
