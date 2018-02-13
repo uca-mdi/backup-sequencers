2fbc4bc267aed56568cbb55ba2bce5b963de4558
marcomilanesio <milanesio.marco@gmail.com>
2018-02-13 11:41:46 +0100

    setup script

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SCRIPT='./bkptest.sh'
+NAME=
+
+if [ -z "$1" ]; then
+    echo -e "Usage: '$0 <name of server>'\n"
+    exit 1
+else
+    NAME=$1
+fi
+
+if [ ! -f ${SCRIPT} ]; then
+	echo 'file ${SCRIPT} not found. Aborting'
+	exit 1
+else
+	sed -i 's/--dry-run//g' ${SCRIPT}
+	sed -i 's/^WHO=\"\"/WHO=\"'$NAME'\"/g' ${SCRIPT}
+	echo 'done'
+fi
